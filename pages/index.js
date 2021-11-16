12b6f3843a7b880d035d59f4753f6a460f6696a2
tal0311 <tal.amit0311@gmail.com>
2021-11-16 21:45:04 +0200

    #

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 const homePage = () => {
   return (
     <div>
       <h1>the home page</h1>
+
+      <ul>
+        <li>
+          <Link href=''></Link>
+        </li>
+      </ul>
     </div>
   )
 }
